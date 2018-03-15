4ed992dda668b14370e4fb20bc77bba87e861ef0
Emre <emrsmsrli1@gmail.com>
2018-03-15 20:14:21 +0300

    implement PersonList to satisfy generics and ADT usage

---
+++ HospitalManagement/src/hospitalmanagement/util/PersonList.java
@@ -0,0 +1,38 @@
+package hospitalmanagement.util;
+
+import hospitalmanagement.people.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class PersonList<T extends Person> {
+    private List<T> people;
+
+    public PersonList() {
+        people = new ArrayList<>();
+    }
+
+    public void add(T person) {
+        people.add(person);
+    }
+
+    public void removeAt(T person) {
+        people.remove(person);
+    }
+
+    public int size() {
+        return people.size();
+    }
+
+    public void forEach(Consumer<T> consumer) {
+        for(T t : people) {
+            consumer.accept(t);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return people.toString();
+    }
+}
