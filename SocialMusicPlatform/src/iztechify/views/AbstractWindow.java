30fc5f3da09bf42bfd585cb42962205fee5b4bf3
Emre <emrsmsrli1@gmail.com>
2018-04-16 16:14:27 +0300

    abstract window

---
+++ SocialMusicPlatform/src/iztechify/views/AbstractWindow.java
@@ -0,0 +1,9 @@
+package iztechify.views;
+
+import javax.swing.*;
+
+public class AbstractWindow extends JFrame implements Window {
+    public AbstractWindow(String title) {
+        super(title);
+    }
+}
