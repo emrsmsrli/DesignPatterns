afb350ad5f71c0f47582ceb61f9b9c10bdb70b7d
BahadirYurdakul <bahadiryurdakul@gmail.com>
2018-04-17 17:03:07 +0300

    Popularity level removed.

--- SocialMusicPlatform/src/iztechify/models/music/Artist.java
+++ SocialMusicPlatform/src/iztechify/models/music/Artist.java
@@ -7,23 +7,9 @@ import java.util.Observable;
 public class Artist extends Observable {
     private String name;
     private List<Album> albums;
-    private int popularityLevel;
 
     public Artist(String name) {
         this.name = name;
         this.albums = new ArrayList<>();
-        popularityLevel = 0;
-    }
-
-    private int getPopularityLevel(){
-        return popularityLevel;
-    }
-
-    public int incrementPopularityLevel(){
-        return ++popularityLevel;
-    }
-
-    public int decrementPopularityLevel(){
-        return --popularityLevel;
     }
 }
--- SocialMusicPlatform/src/iztechify/models/music/Song.java
+++ SocialMusicPlatform/src/iztechify/models/music/Song.java
@@ -5,20 +5,14 @@ import java.util.Observable;
 public class Song extends Observable {
     private String title;
     private String length;
-    private int popularityLevel;
 
     public Song(String title, String length) {
         this.title = title;
         this.length = length;
-        popularityLevel = 0;
     }
 
     public void setTitle(String newTitle){
         title = newTitle;
         notifyObservers();
-    }
-
-    public int getPopularityLevel(){
-        return popularityLevel;
     }
 }
