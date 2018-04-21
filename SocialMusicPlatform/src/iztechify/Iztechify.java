a2a05cf555e886d4feb8225eb4c0038fb78ae19e
Emre <emrsmsrli1@gmail.com>
2018-04-21 15:40:55 +0300

    fix Music() constructor in main()

--- SocialMusicPlatform/src/iztechify/Iztechify.java
+++ SocialMusicPlatform/src/iztechify/Iztechify.java
@@ -2,12 +2,15 @@ package iztechify;
 
 import iztechify.controllers.LoginController;
 import iztechify.models.music.Music;
+import iztechify.util.GSON;
 import iztechify.views.LoginWindow;
 import iztechify.views.Window;
 
+import java.util.Arrays;
+
 public class Iztechify {
     public static void main(String... args) {
-        Music music = new Music();
+        Music music = new Music(Arrays.asList(GSON.loadMusic()));
         LoginController loginController = new LoginController(music);
         Window loginWindow = new LoginWindow(music, loginController);
         loginWindow.showWindow();
