16f2a60c06c795b09036c614b9c41b55be063920
Emre <emrsmsrli1@gmail.com>
2018-04-22 20:09:15 +0300

    gson refactor, allow circularity

--- SocialMusicPlatform/src/iztechify/util/GSON.java
+++ SocialMusicPlatform/src/iztechify/util/GSON.java
@@ -3,6 +3,8 @@ package iztechify.util;
 import com.google.gson.Gson;
 import iztechify.models.Music;
 import iztechify.models.Users;
+import iztechify.models.music.Album;
+import iztechify.models.music.Song;
 import iztechify.models.user.User;
 import iztechify.models.music.Artist;
 
@@ -11,7 +13,39 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.*;
 
+@SuppressWarnings("all")
 public class GSON implements Observer {
+    private static class _Artist {
+        String name;
+        List<_Album> albums;
+        public _Artist() {}
+        public _Artist(String name, List<_Album> albums) {
+            this.name = name;
+            this.albums = albums;
+        }
+    }
+    private static class _Album {
+        String title;
+        String description;
+        List<_Song> songs;
+
+        public _Album() {}
+        public _Album(String title, String description, List<_Song> songs) {
+            this.title = title;
+            this.description = description;
+            this.songs = songs;
+        }
+    }
+    private static class _Song {
+        String title;
+        String length;
+        public _Song() {}
+        public _Song(String title, String length) {
+            this.title = title;
+            this.length = length;
+        }
+    }
+
     private static final Gson _GSON = new Gson();
     private static final String FILE_NAME_MUSIC = "music.json";
     private static final String FILE_NAME_USER = "user.json";
@@ -19,7 +53,21 @@ public class GSON implements Observer {
 
     public List<Artist> loadMusic() {
         try(FileReader fr = new FileReader(FILE_NAME_MUSIC)) {
-            return new ArrayList<>(Arrays.asList(_GSON.fromJson(fr, Artist[].class)));
+            List<_Artist> loadedArtists = new ArrayList<>(Arrays.asList(_GSON.fromJson(fr, _Artist[].class)));
+
+            List<Artist> artists = new ArrayList<>();
+            for(_Artist loadedArtist : loadedArtists) {
+                Artist artist = new Artist(loadedArtist.name);
+                for(_Album loadedAlbum : loadedArtist.albums) {
+                    Album album = new Album(artist, loadedAlbum.title, loadedAlbum.description);
+                    for(_Song loadedSong : loadedAlbum.songs)
+                        album.addSong(new Song(album, loadedSong.title, loadedSong.length));
+                    artist.addAlbum(album);
+                }
+                artists.add(artist);
+            }
+
+            return artists;
         } catch(Exception e) { e.printStackTrace(); throw new RuntimeException(); } // prevent "return null"
     }
 
@@ -33,8 +81,19 @@ public class GSON implements Observer {
         } catch(Exception e) { e.printStackTrace(); throw new RuntimeException(); }
     }
 
-    private void saveMusic(List<Artist> artists) {
+    private void saveMusic(List<Artist> arr) {
         try(FileWriter fw = new FileWriter(FILE_NAME_MUSIC)) {
+            List<_Artist> artists = new ArrayList<>();
+            for(Artist artist : arr) {
+                List<_Album> albums = new ArrayList<>();
+                for(Album album : artist.getAlbums()) {
+                    List<_Song> songs = new ArrayList<>();
+                    for(Song song : album.getSongs())
+                        songs.add(new _Song(song.getTitle(), song.getLength()));
+                    albums.add(new _Album(album.getTitle(), album.getDescription(), songs));
+                }
+                artists.add(new _Artist(artist.getName(), albums));
+            }
             fw.write(_GSON.toJson(artists));
         } catch(Exception e) { e.printStackTrace(); }
     }
