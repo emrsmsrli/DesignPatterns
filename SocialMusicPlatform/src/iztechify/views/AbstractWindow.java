83741cffca23815dce13bc26d36c222a41370696
Emre <emrsmsrli1@gmail.com>
2018-04-22 15:10:26 +0300

    override update() on abstractWindow since some classes won't be using it

--- SocialMusicPlatform/src/iztechify/views/AbstractWindow.java
+++ SocialMusicPlatform/src/iztechify/views/AbstractWindow.java
@@ -1,10 +1,14 @@
 package iztechify.views;
 
 import javax.swing.*;
+import java.util.Observable;
 import java.util.Observer;
 
 public abstract class AbstractWindow extends JFrame implements Window, Observer {
     public AbstractWindow(String title) {
         super(title);
     }
+
+    @Override
+    public void update(Observable o, Object arg) { }
 }
