84094bd36b404de5e5b2082fe1c0e4155c79c0c6
Emre <emrsmsrli1@gmail.com>
2018-05-12 13:18:19 +0300

    - refactor logger - logger now uses a file (log.txt) instead of stdout

--- CJXY/src/cjxy/util/Logger.java
+++ CJXY/src/cjxy/util/Logger.java
@@ -1,30 +1,45 @@
 package cjxy.util;
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Logger {
     private static Logger instance = null;
-    private static Calendar cal;
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("[yyyy.MM.dd, HH:mm:ss]");
+
+    private Writer bw = null;
+    private final Calendar cal = Calendar.getInstance();
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("[yyyy/MM/dd][HH:mm:ss]");
 
     private Logger() {
-        cal = Calendar.getInstance();
+        try {
+            bw = new FileWriter(new File("log.txt"));
+            bw.write("CJXY - Data Exchange Format Converter");
+            bw.write(time() + " [Logging Started]");
+        } catch(IOException ignored) {}
     }
 
-    public static Logger getInstance() {
+    public void info(String message) {
+        String log = time() + " [" + message + "]" + " [SUCCESSFUL]";
+        try {
+            bw.write(log);
+        } catch(Exception ignored) {}
+    }
+
+    public void error(String message) {
+        String log = time() + " [" + message + "]" + " [FAILED]";
+        try {
+            bw.write(log);
+        } catch(Exception ignored) {}
+    }
+
+    private String time() {
+        return dateFormat.format(cal.getTime());
+    }
+
+    public static Logger get() {
         if(instance == null)
             instance = new Logger();
         return instance;
-    }
-
-    public void logInfoWithTime(String info) {
-        String log = dateFormat.format(cal.getTime()) + " [" + info + "]" + " [SUCCESSFUL]";
-        System.out.println(log);
-    }
-
-    public void logErrorWithTime(String info) {
-        String log = dateFormat.format(cal.getTime()) + " [" + info + "]" + " [FAILED]";
-        System.out.println(log);
     }
 }
