2ff1352396f9fc9a969b580ddeef006f4837cb6e
Emre <emrsmsrli1@gmail.com>
2018-05-13 15:47:44 +0300

    xml read impl with xstream

--- CJXY/src/cjxy/converters/XmlConverter.java
+++ CJXY/src/cjxy/converters/XmlConverter.java
@@ -1,14 +1,42 @@
 package cjxy.converters;
 
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class XmlConverter extends Converter {
+    public static class MapEntryConverter implements com.thoughtworks.xstream.converters.Converter {
+        public boolean canConvert(Class clazz) {
+            return AbstractMap.class.isAssignableFrom(clazz);
+        }
+        public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) { }
+
+        public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+            Map<String, Object> map = new HashMap<>();
+            while(reader.hasMoreChildren()) {
+                reader.moveDown();
+                String key = reader.getNodeName(); // nodeName aka element's name
+                Object value = reader.getValue();
+                map.put(key, value);
+                reader.moveUp();
+            }
+            return map;
+        }
+    }
+
     private static XmlConverter instance;
+    private XStream xStream;
 
     private XmlConverter() {
-        mapper = new XmlMapper();
+        xStream = new XStream();
+        xStream.alias("root", Map.class);
+        xStream.registerConverter(new MapEntryConverter());
     }
 
     @Override
@@ -32,8 +60,8 @@ public class XmlConverter extends Converter {
     }
 
     @Override
-    public Object read(String data) throws Exception {
-        return mapper.readValue(data, Map.class);
+    public Object read(String data) {
+        return xStream.fromXML(data);
     }
 
     @Override
