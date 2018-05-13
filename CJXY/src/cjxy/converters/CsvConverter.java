6ed60c4e7897481d7a9bfa3b08dce4a4314a71f4
Emre <emrsmsrli1@gmail.com>
2018-05-13 13:12:06 +0300

    - fix csv writing. csv write supports only two cases.

--- CJXY/src/cjxy/converters/CsvConverter.java
+++ CJXY/src/cjxy/converters/CsvConverter.java
@@ -3,7 +3,9 @@ package cjxy.converters;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class CsvConverter extends Converter {
     private static CsvConverter instance;
@@ -40,8 +42,26 @@ public class CsvConverter extends Converter {
 
     @Override
     public String write(Object data) throws Exception {
-        // TODO
-        return mapper.writeValueAsString(data);
+        if(data instanceof List) {
+            Object first = ((List) data).get(0);
+            if(!(first instanceof Map))
+                return mapper.writeValueAsString(data);
+            return write(first, data);
+        } else if(data instanceof Map) {
+            return write(data, data);
+        } else
+            return mapper.writeValueAsString(data);
+    }
+
+    private String write(Object hdrs, Object data) throws Exception {
+        Set headers = ((Map)hdrs).keySet();
+        CsvSchema.Builder schemaBuilder = CsvSchema.builder();
+        schemaBuilder.setUseHeader(true);
+        for(Object header : headers) {
+            schemaBuilder.addColumn((String) header);
+        }
+        CsvSchema schema = schemaBuilder.build();
+        return mapper.writer().with(schema).writeValueAsString(data);
     }
 
     public static CsvConverter get() {
